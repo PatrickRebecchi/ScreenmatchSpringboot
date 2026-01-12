60bf8a14c29d15cc9096b325a49cea588d71b5bb
Patrick <patrickrebecchi@hotmail.com>
2026-01-12 20:33:45 -0300

    Feat: Criando projeto em Spring

---
+++ src/main/java/br/com/alura/screenmatchSpring/ScreenmatchSpringApplication.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatchSpring;
+
+import br.com.alura.screenmatchSpring.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        ConsumoAPI api = new ConsumoAPI();
+        var json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=6585022c");
+        System.out.println(json);
+    }
+}
