f6d0088f0916b9e64d7fcc9c8d1096e6fa19bcfb
Patrick <patrickrebecchi@hotmail.com>
2026-01-12 21:59:40 -0300

    Feat, dados do episodio e usando o generic de converte dados serie para dados episodio

--- src/main/java/br/com/alura/screenmatchSpring/ScreenmatchSpringApplication.java
+++ src/main/java/br/com/alura/screenmatchSpring/ScreenmatchSpringApplication.java
@@ -1,6 +1,9 @@
 package br.com.alura.screenmatchSpring;
 
+import br.com.alura.screenmatchSpring.model.DadosEpisodio;
+import br.com.alura.screenmatchSpring.model.DadosSerie;
 import br.com.alura.screenmatchSpring.service.ConsumoAPI;
+import br.com.alura.screenmatchSpring.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +20,12 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
         ConsumoAPI api = new ConsumoAPI();
         var json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
         System.out.println(json);
+
+        ConverteDados converso = new ConverteDados();
+        DadosSerie dados = converso.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+        json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=6585022c");
+        DadosEpisodio dadosEpisodio = converso.obterDados(json, DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
     }
 }
