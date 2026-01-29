b55bd1cacec6c2da64d1b1fa19584b89239db7ce
Patrick <patrickrebecchi@hotmail.com>
2026-01-28 22:51:31 -0300

    conectando a api com o chat GPT e o gemini para traduzir a sinopse

--- src/main/java/br/com/alura/screenmatchSpring/model/Serie.java
+++ src/main/java/br/com/alura/screenmatchSpring/model/Serie.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatchSpring.model;
 
 import br.com.alura.screenmatchSpring.service.ConsultaChatGPT;
+import br.com.alura.screenmatchSpring.service.ConsultaGemini;
 
 import java.util.OptionalDouble;
 
@@ -76,7 +77,8 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = ConsultaChatGPT
+                .obterTraducao(dadosSerie.sinopse()).trim();
     }
 
     @Override
--- src/main/java/br/com/alura/screenmatchSpring/service/ConsultaGemini.java
+++ src/main/java/br/com/alura/screenmatchSpring/service/ConsultaGemini.java
@@ -1,20 +1,19 @@
 package br.com.alura.screenmatchSpring.service;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 
-public class ConsultaChatGPT {
+public class ConsultaGemini {
 
     public static String obterTraducao(String texto) {
 
-        ChatLanguageModel chatGPT = OpenAiChatModel.builder()
-                .apiKey(System.getenv("OPENAI_API_KEY"))
-                //.modelName("gpt-4o-mini")
-                .modelName("text-davinci-003")
-                .temperature(0.0)
+        ChatLanguageModel gemini = GoogleAiGeminiChatModel.builder()
+                .apiKey(System.getenv("API_KEY_GEMINI"))
+                .modelName("gemini-2.5-flash")
                 .build();
 
-        String response = chatGPT.generate(
+        // Adicionamos uma instrução direta para evitar conversas
+        String response = gemini.generate(
                 "Traduza o seguinte texto para o português. " +
                         "Retorne apenas a tradução direta, sem comentários, sem explicações e sem variações: " + texto
         );
