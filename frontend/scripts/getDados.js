d631a12a4999d476758b45296cf9951df35888cd
Patrick <patrickrebecchi@hotmail.com>
2026-02-24 23:00:46 -0300

    subindo alteração

--- frontend/scripts/getDados.js
+++ frontend/scripts/getDados.js
@@ -1,10 +1,26 @@
 // URL base da API
-const baseURL = 'http://localhost:8080';
+// const baseURL = 'http://localhost:8080';
+
+//export default function getDados(endpoint) {
+  //  return fetch(`${baseURL}${endpoint}`)
+     //   .then(response => response.json())
+   //     .catch(error => {
+ //           console.error('Erro ao acessar o endpoint /series/top5:', error);
+//        });
+//}
+
+// URL base da API (produção)
+const baseURL = 'https://screenmatchspringboot.onrender.com';
 
 export default function getDados(endpoint) {
     return fetch(`${baseURL}${endpoint}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro HTTP: ${response.status}`);
+            }
+            return response.json();
+        })
         .catch(error => {
-            console.error('Erro ao acessar o endpoint /series/top5:', error);
+            console.error('Erro ao acessar API:', error);
         });
 }
